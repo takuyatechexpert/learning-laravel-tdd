e1b9b58502f48c4ea052860d3ee5195f0e0a00f7
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-10-11 00:32:40 +0900

    add lesson and reservation

---
+++ app/Http/Controllers/ModelsReservationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModelsReservation;
+use Illuminate\Http\Request;
+
+class ModelsReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModelsReservation  $modelsReservation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelsReservation $modelsReservation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModelsReservation  $modelsReservation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelsReservation $modelsReservation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModelsReservation  $modelsReservation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelsReservation $modelsReservation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModelsReservation  $modelsReservation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelsReservation $modelsReservation)
+    {
+        //
+    }
+}
---
+++ database/factories/ModelsReservationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ModelsReservation;
+use Faker\Generator as Faker;
+
+$factory->define(ModelsReservation::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
