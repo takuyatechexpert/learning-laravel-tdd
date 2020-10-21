f29437ed48327d2f030b6c50a563cca7fc6540eb
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-10-21 10:04:02 +0900

    add test

--- tests/Feature/Http/Controllers/LessonControllerTest.php
+++ tests/Feature/Http/Controllers/LessonControllerTest.php
@@ -18,9 +18,10 @@ class LessonControllerTest extends TestCase
    * @param int $capacity
    * @param int $reservationCount
    * @param string $expectedVacancyLevelMark
+   * @param string $button
    * @dataProvider dataShow
    */
-    public function testShow(int $capacity, int $reservationCount, string $expectedVacancyLevelMark)
+  public function testShow(int $capacity, int $reservationCount, string $expectedVacancyLevelMark, string $button)
     {
 
         $lesson = factory(Lesson::class)->create(['name' => '楽しいヨガレッスン', 'capacity' => $capacity]);
@@ -33,25 +34,33 @@ class LessonControllerTest extends TestCase
         $response->assertStatus(Response::HTTP_OK);
         $response->assertSee($lesson->name);
         $response->assertSee("空き状況: {$expectedVacancyLevelMark}");
+
+        $response->assertSee($button, false);
     }
 
     public function dataShow()
     {
+        $button = '<button class="btn btn-primary">このレッスンを予約する</button>';
+        $span = '<span class="btn btn-primary disabled">予約できません</span>';
+
         return [
             '空き十分' => [
                 'capacity' => 6,
                 'reservationCount' => 1,
                 'expectedVacancyLevelMark' => '◎',
+                'button' => $button,
             ],
             '空きわずか' => [
                 'capacity' => 6,
                 'reservationCount' => 2,
                 'expectedVacancyLevelMark' => '△',
+                'button' => $button,
             ],
             '空きなし' => [
                 'capacity' => 1,
                 'reservationCount' => 1,
                 'expectedVacancyLevelMark' => '×',
+                'button' => $span,
             ],
         ];
     }
