6d310f19251ff2d6e88e8df1048de04f2fe199e7
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-11-05 09:43:18 +0900

    add lesson policy and fix show view

--- app/Policies/LessonPolicy.php
+++ app/Policies/LessonPolicy.php
@@ -2,20 +2,22 @@
 
 namespace App\Policies;
 
+use App\Models\Lesson;
 use App\Models\User;
+use Exception;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class LessonPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function reserve(User $user, Lesson $lesson): bool
     {
-        //
+        try {
+            $user->canReserve($lesson);
+        } catch (Exception $e) {
+            return false;
+        }
+        return true;
     }
 }
