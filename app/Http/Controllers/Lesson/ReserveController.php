f99820fe7313e0733bf5aec31ec85ba3ea0d0f23
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-11-12 10:06:30 +0900

    add reservation create

--- app/Http/Controllers/Lesson/ReserveController.php
+++ app/Http/Controllers/Lesson/ReserveController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Lesson;
 
 use App\Http\Controllers\Controller;
 use App\Models\Lesson;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
 
 class ReserveController extends Controller
 {
@@ -15,6 +17,9 @@ class ReserveController extends Controller
      */
     public function __invoke(Lesson $lesson)
     {
+        $user = Auth::user();
+        Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
+
         // TODO
         // 予約
         return redirect()->route('lessons.show', ['lesson' => $lesson]);
